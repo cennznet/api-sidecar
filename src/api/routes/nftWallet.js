72c57a17d1db0822602979c5fa217e6a34589151
holygits <beauchjord@gmail.com>
2021-10-05 14:27:33 +1300

    more tidy up

--- src/api/routes/nftWallet.js
+++ src/api/routes/nftWallet.js
@@ -1,5 +1,7 @@
+const { NFT_WALLETS_COLLECTION } = require('../../mongo/schema');
+
 async function routes (fastify, options) {
-    const collection = fastify.mongo.db.collection('nftwallets')
+    const collection = fastify.mongo.db.collection(NFT_WALLETS_COLLECTION)
 
     fastify.get('/nftWallet/:address', async (request, reply) => {
       const result = await collection.findOne({ _id: request.params.address })
--- src/mongo/schema.js
+++ src/mongo/schema.js
@@ -1,14 +1,18 @@
 const { Schema } = require('mongoose');
 
+const NFT_WALLETS_COLLECTION = 'nftwallets';
+
 const NftWalletSchema = new Schema({
     _id: String,
     tokens: [{
         collectionId: Number,
         seriesId: Number,
         serialNumber: Number,
-    }]
-})
+    }],
+    NFT_WALLETS_COLLECTION,
+});
 
 module.exports = {
-    NftWalletSchema
+    NftWalletSchema,
+    NFT_WALLETS_COLLECTION,
 }
