d96f43b80ff36fbbf2a7621df33fbe09ec6fe323
holygits <beauchjord@gmail.com>
2021-10-05 15:36:58 +1300

    Add schema for listings + indexes

--- src/api/routes/nftWallet.js
+++ src/api/routes/nftWallet.js
@@ -1,10 +1,21 @@
-const { NFT_WALLETS_COLLECTION } = require('../../mongo/schema');
+const { NFT_LISTINGS_COLLECTION, NFT_WALLETS_COLLECTION } = require('../../mongo/models');
 
 async function routes (fastify, options) {
-    const collection = fastify.mongo.db.collection(NFT_WALLETS_COLLECTION)
+    const collectionWallet = fastify.mongo.db.collection(NFT_WALLETS_COLLECTION);
+    const collectionListings = fastify.mongo.db.collection(NFT_LISTINGS_COLLECTION);
 
-    fastify.get('/nftWallet/:address', async (request, reply) => {
-      const result = await collection.findOne({ _id: request.params.address })
+    fastify.get('/nft/wallet/:address', async (request, reply) => {
+      // TODO: suppress _id
+      const result = await collectionWallet.findOne({ _id: request.params.address });
+      if (!result) {
+        throw new Error('invalid value')
+      }
+      return result
+    })
+
+    fastify.get('/nft/listing/:listingId', async (request, reply) => {
+      // TODO: suppress _id
+      const result = await collectionListings.findOne({ _id: request.params.listingId });
       if (!result) {
         throw new Error('invalid value')
       }
---
+++ src/mongo/models.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+
+const NFT_WALLETS_COLLECTION = 'nftwallets';
+const NFT_LISTINGS_COLLECTION = 'nftlistings'
+
+const NftWalletSchema = new Schema({
+    _id: String, // CENNZnet ss58 address
+    activeListings: [String], // u128 listings
+    tokens: [{
+        collectionId: Number,
+        seriesId: Number,
+        serialNumber: Number,
+    }],
+}, { collection: NFT_WALLETS_COLLECTION });
+NftWalletSchema.index({ tokens: 1 });
+
+const NftListingSchema = new Schema({
+    _id: String, // u128 is too large for js Number type
+    seller: String, // CENNZnet ss58 address
+    tokens: [{
+        collectionId: Number,
+        seriesId: Number,
+        serialNumber: Number,
+    }],
+}, { collection: NFT_LISTINGS_COLLECTION });
+NftListingSchema.index({ tokens: 1 });
+
+module.exports = {
+    NftListing: mongoose.model('NftListing', NftListingSchema),
+    NftWallet: mongoose.model('NftWallet', NftWalletSchema),
+    NFT_LISTINGS_COLLECTION,
+    NFT_WALLETS_COLLECTION,
+}
