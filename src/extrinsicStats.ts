c65d6cd65ce8cb47eed7a4d14019dbc48f908872
Eug <eupston130@hotmail.com>
2022-04-28 16:46:34 +1200

    updated with creating graphs to for section and method extrinsic stats

--- src/extrinsicStats.ts
+++ src/extrinsicStats.ts
@@ -1,5 +1,26 @@
 import axios from "axios";
 import { createObjectCsvWriter as createCSV } from "csv-writer";
+const fs = require('fs');
+const request = require('request');
+
+interface SectionsChartData {
+	sections: string[];
+	failedExts: number[];
+	successfulExts: number[];
+}
+
+interface SectionMethodsChartData {
+	section: string;
+	methods: string[];
+	failedExts: number[];
+	successfulExts: number[];
+}
+
+interface SectionData {
+	section: string;
+	successfulExt: string;
+	failedExt: string;
+}
 
 async function main(startDate, endDate, fileName) {
 	let start = new Date(startDate);
@@ -65,28 +86,20 @@ async function main(startDate, endDate, fileName) {
 	while (start <= end) {
 		const dateTimeInParts = start.toISOString().split("T");
 		const date = dateTimeInParts[0];
-		console.log(date);
+		console.log("date:", date);
 		await Promise.all(
 			sections.map(async (section) => {
 				const mtds = methods[section];
-				console.log("mtds::", mtds);
 				await Promise.all(
 					mtds.map(async (mtd) => {
 						const csvObject = {};
 						csvObject["date"] = date;
 						const extrinsic = `${section}.${mtd}`;
-						console.log("extrinsic::", extrinsic);
 						csvObject["extrinsic"] = extrinsic;
 						const response = await axios.get(
 							`https://service.eks.centrality.cloud/cennznet-explorer-api/api/scan/extrinsics_count?section=${section}&method=${mtd}&date=${date}`
 						);
 						const [successfulExt, failedExt] = response?.data?.data;
-						console.log("successfulExt:", successfulExt);
-						console.log("failedExt:", failedExt);
-						console.log(
-							`successfulExt of type ${section}:: is ${successfulExt}`
-						);
-						console.log(`FailedExt of type ${section}:: is ${failedExt}`);
 						csvObject["successfulExt"] = successfulExt;
 						csvObject["failedExt"] = failedExt;
 						csvArray.push(csvObject);
@@ -97,8 +110,6 @@ async function main(startDate, endDate, fileName) {
 		let newDate = start.setDate(start.getDate() + 1);
 		start = new Date(newDate);
 	}
-	console.log("csvObject::", csvArray);
-	console.log("csvObject::", `${fileName}.csv`);
 	const csv = createCSV({
 		path: `${fileName}.csv`,
 		header: [
@@ -108,7 +119,65 @@ async function main(startDate, endDate, fileName) {
 			{ id: "failedExt", title: "Failed Txs" },
 		],
 	});
-
+	const sectionAggregation = {};
+	csvArray.forEach(extEntry => {
+		const sectionName = extEntry.extrinsic.split(".")[0];
+		const sectionMethodName = extEntry.extrinsic.split(".")[1];
+		if(sectionAggregation[sectionName]){
+			const successfulEx = sectionAggregation[sectionName].successfulExt + extEntry.successfulExt;
+			const failedEx = sectionAggregation[sectionName].failedExt + extEntry.failedExt;
+			sectionAggregation[sectionName] =  {
+				...sectionAggregation[sectionName],
+				successfulExt: successfulEx,
+				failedExt: failedEx,
+			}
+			if(sectionAggregation[sectionName][sectionMethodName]){
+				const successfulEx = sectionAggregation[sectionName][sectionMethodName].successfulExt + extEntry.successfulExt;
+				const failedEx = sectionAggregation[sectionName][sectionMethodName].failedExt + extEntry.failedExt;
+				sectionAggregation[sectionName][sectionMethodName] = {
+					successfulExt: successfulEx,
+					failedExt: failedEx
+				}
+			}
+			else{
+				sectionAggregation[sectionName][sectionMethodName] = {
+					successfulExt: extEntry.successfulExt,
+					failedExt: extEntry.failedExt
+				}
+			}
+		}
+		else {
+			sectionAggregation[sectionName] = {
+				successfulExt: extEntry.successfulExt,
+				failedExt: extEntry.failedExt,
+				[sectionMethodName]: {
+					successfulExt: extEntry.successfulExt,
+					failedExt: extEntry.failedExt,
+				}
+			};
+		}
+	});
+	console.info("sectionAggregation", sectionAggregation)
+	const sects = Object.keys(sectionAggregation);
+	const sectionsChartData: SectionsChartData = {
+		sections: sects,
+		successfulExts: sects.map(section => sectionAggregation[section].successfulExt),
+		failedExts: sects.map(section => sectionAggregation[section].failedExt)
+	}
+	await createSectionsGraph(sectionsChartData);
+	const sectionMethodsGraphDatas: SectionMethodsChartData[] = sects.map(section => {
+		const methods = Object.keys(sectionAggregation[section]).filter(method => method !== "successfulExt" && method !== "failedExt");
+		const successfulExts = methods.map(method => sectionAggregation[section][method].successfulExt );
+		const failedExts = methods.map(method => sectionAggregation[section][method].failedExt);
+		return {
+			section,
+			methods,
+			failedExts,
+			successfulExts
+		}
+	});
+	const createSectionsGraphProms = sectionMethodsGraphDatas.map(async chartData => {return await createSectionMethodsGraph(chartData);});
+	await Promise.all(createSectionsGraphProms);
 	csv
 		.writeRecords(
 			csvArray
@@ -120,6 +189,138 @@ async function main(startDate, endDate, fileName) {
 			console.log("Done!");
 		});
 }
+
+const createSectionMethodsGraph = (sectionMethodsData: SectionMethodsChartData) => {
+	const quickChartURL = 'https://quickchart.io/chart/create';
+	const post_data = {
+		chart: {
+			type: 'bar',
+			data: {
+				'labels': sectionMethodsData.methods,
+				datasets: [
+					{
+						label: 'Successful Extrinsics',
+						backgroundColor: 'rgb(75, 192, 192)',
+						data: sectionMethodsData.successfulExts,
+					},
+					{
+						label: 'Failed Extrinsics',
+						backgroundColor: 'rgb(255, 99, 132)',
+						data: sectionMethodsData.failedExts,
+					},
+				]
+			},
+			options: {
+				title: {
+					display: true,
+					text: `CENNZnet ${sectionMethodsData.section} Module Transactions November`,
+				},
+				scales: {
+					xAxes: [
+						{
+							stacked: false,
+						},
+					],
+					yAxes: [
+						{
+							stacked: false,
+						},
+					],
+				},
+			},
+		}
+	}
+	const createChartProm = new Promise((resolve, reject) => {
+		request.get({ url: quickChartURL, method: "POST", json: true, body: post_data }, function (error, response, body) {
+			if (!error) {
+				const filePath = "src/charts/"
+				if (!fs.existsSync(filePath)){
+					fs.mkdirSync(filePath);
+				}
+				request(body['url']).pipe(fs.createWriteStream(`${sectionMethodsData.section}.png`)).on('close', () => {
+					resolve(body['url']);
+				});
+			} else {
+				reject(response);
+			}
+		});
+	})
+	createChartProm
+		.then(chartUrl => {
+			console.log(chartUrl)
+		})
+		.catch((err) => {
+			console.error(err)
+			return false;
+		})
+	return true;
+}
+
+const createSectionsGraph = (sectionsData: SectionsChartData) => {
+	const quickChartURL = 'https://quickchart.io/chart/create'
+	const post_data = {
+		chart: {
+			type: 'bar',
+			data: {
+				'labels': sectionsData.sections,
+				datasets: [
+					{
+						label: 'Successful Extrinsics',
+						backgroundColor: 'rgb(75, 192, 192)',
+						data: sectionsData.successfulExts,
+					},
+					{
+						label: 'Failed Extrinsics',
+						backgroundColor: 'rgb(255, 99, 132)',
+						data: sectionsData.failedExts,
+					},
+				]
+			},
+			options: {
+				title: {
+					display: true,
+					text: 'CENNZnet Module Transactions November',
+				},
+				scales: {
+					xAxes: [
+						{
+							stacked: false,
+						},
+					],
+					yAxes: [
+						{
+							stacked: false,
+						},
+					],
+				},
+			},
+		}
+	}
+	const createChartProm = new Promise((resolve, reject) => {
+		request.get({ url: quickChartURL, method: "POST", json: true, body: post_data }, function (error, response, body) {
+			if (!error) {
+				const filePath = "src/charts/"
+				if (!fs.existsSync(filePath)){
+					fs.mkdirSync(filePath);
+				}
+				request(body['url']).pipe(fs.createWriteStream("Test.png")).on('close', () => {
+					resolve(body['url']);
+				});
+			} else {
+				reject(response);
+			}
+		});
+	})
+	createChartProm
+		.then(chartUrl => {
+			console.log(chartUrl)
+		})
+		.catch((err) => {
+			console.error(err)
+			return false;
+		})
+	return true;
+};
 
 main(process.argv[2], process.argv[3], process.argv[4]).catch((err) =>
 	console.log(err)
