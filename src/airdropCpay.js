03058c1c406c32827012487c645ae6dca8c5880e
KarishmaBothara <bothara.karishma@gmail.com>
2022-06-15 15:31:13 +1200

    check if data already proccessed on restart of service

--- src/airdropCpay.js
+++ src/airdropCpay.js
@@ -10,44 +10,57 @@ async function main () {
     const api = await Api.create({network: networkName});
     console.log(`Connect to cennznet network ${networkName}`);
     try {
-        const data = fs.readFileSync('src/payouts.csv', 'utf8')
+        const data = fs.readFileSync('src/payouts.csv', 'utf8');
+        const processeddata = fs.readFileSync('src/payoutsWithTxHash.csv', 'utf8');
+        const processedCsvData = processeddata.split("\n");
         const spendingAssetId = (await api.query.genericAsset.spendingAssetId()).toNumber();
         console.log('spending asset id:', spendingAssetId);
         const csvData = data.split("\n");
         console.log('arr.length:',csvData.length);
         // const sudoAddress = await api.query.sudo.key();
         const keyring = new Keyring({type: 'sr25519'});
-        const airdropAccount = keyring.addFromSeed(process.ENV.SEED);
+        const airdropAccount = keyring.addFromSeed(seed);
         let nonce = await api.rpc.system.accountNextIndex(airdropAccount.address);
 
-        const newCSVDataHeader = "Account, 8329 Era, 8339 Era, 8357 Era, 8391 Era, 8396 Era, Total, TransactionHash";
-        fs.appendFile("src/payoutsWithTxHash.csv", newCSVDataHeader, (err)=> {if (err) { console.error('error inserting data in csv'); }});
+        // need to append the header only once
+        // const newCSVDataHeader = "Account, 8329 Era, 8339 Era, 8357 Era, 8391 Era, 8396 Era, Total, TransactionHash";
+        // fs.appendFile("src/payoutsWithTxHash.csv", newCSVDataHeader, (err)=> {if (err) { console.error('error inserting data in csv'); }});
         for (let records = 1; records < csvData.length; records++) {
             const record = csvData[records]
             const [receiver, era1, era2, era3, era4, era5, totalAmount] = record.split(',');
-
-            await new Promise((resolve) => {
-                const tx = api.tx.genericAsset.transfer(spendingAssetId, receiver, totalAmount);
-                tx.signAndSend(airdropAccount, {nonce: nonce++}, async ({events, status}) => {
-                    console.log('status:',status.toHuman());
-                    if (status.isInBlock) {
-                        for (const {event: {method, section, data}} of events) {
-                            if (section === 'genericAsset' && method == 'Transferred') {
-                                const [assetId, from, to, amountTransferred] = data;
-                                const newCSVDataAdded = `\r\n ${to}, ${era1}, ${era2}, ${era3}, ${era4}, ${era5}, ${amountTransferred}, ${tx.hash.toString()} `;
-                                console.log('newCSVDataAdded:',newCSVDataAdded);
-                                fs.appendFile("src/payoutsWithTxHash.csv", newCSVDataAdded,
-                                    (err)=> { if (err) { console.error('error inserting data in csv'); }});
-                                resolve();
-                            } else if (section === 'system' && method === 'ExtrinsicFailed') {
-                                fs.appendFile("src/failedPayoutsWithTxHash.csv", `${record}, ${tx.hash.toString()}`,
-                                    (err)=> { if (err) { console.error('error inserting data in csv'); }});
-                                resolve();
+            const recordAlreadyProccessed = processedCsvData.find(data => data.startsWith(` ${receiver}`));
+            if (!recordAlreadyProccessed) {
+                await new Promise((resolve) => {
+                    const tx = api.tx.genericAsset.transfer(spendingAssetId, receiver, totalAmount);
+                    tx.signAndSend(airdropAccount, {nonce: nonce++}, async ({events, status}) => {
+                        console.log('status:', status.toHuman());
+                        if (status.isInBlock) {
+                            for (const {event: {method, section, data}} of events) {
+                                if (section === 'genericAsset' && method == 'Transferred') {
+                                    const [assetId, from, to, amountTransferred] = data;
+                                    const newCSVDataAdded = `\r\n ${to}, ${era1}, ${era2}, ${era3}, ${era4}, ${era5}, ${amountTransferred}, ${tx.hash.toString()} `;
+                                    console.log('newCSVDataAdded:', newCSVDataAdded);
+                                    fs.appendFile("src/payoutsWithTxHash.csv", newCSVDataAdded,
+                                        (err) => {
+                                            if (err) {
+                                                console.error('error inserting data in csv');
+                                            }
+                                        });
+                                    resolve();
+                                } else if (section === 'system' && method === 'ExtrinsicFailed') {
+                                    fs.appendFile("src/failedPayoutsWithTxHash.csv", `${record}, ${tx.hash.toString()}`,
+                                        (err) => {
+                                            if (err) {
+                                                console.error('error inserting data in csv');
+                                            }
+                                        });
+                                    resolve();
+                                }
                             }
                         }
-                    }
+                    });
                 });
-            });
+            }
         }
         console.log('JOB completed!!');
     } catch (err) {
